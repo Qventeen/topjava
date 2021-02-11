c9aced468c130857d056d67a3cb0c46f75f8e455
Sergey Tugaenko <qventeen@gmail.com>
2021-02-11 09:39:45 +0200

    3_4_1 Add logging for repository and service layers

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
@@ -19,7 +22,9 @@ import java.util.function.Predicate;
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
     private final Map<Integer, Map<Integer,Meal>> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
@@ -29,6 +34,7 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Meal save(int userId, Meal meal) {
+        log.info("save {}" , meal);
         //if user id is not contain in repository create new meal map for current user
         Map<Integer, Meal> userMeals = repository.computeIfAbsent(
                 userId,
@@ -44,11 +50,13 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public boolean delete(int userId, int id) {
+        log.info("delete {}", id);
         return repository.get(userId).remove(id) != null;
     }
 
     @Override
     public Meal get(int userId, int id) {
+        log.info("get {}", id);
         return repository.get(userId).get(id);
     }
 
@@ -70,12 +78,13 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Collection<Meal> getByFilter(int userId, Predicate<Meal> filter, Comparator<Meal> comparator) {
-                return repository.get(userId)
-                        .values()
-                        .stream()
-                        .filter(filter)
-                        .sorted(comparator)
-                        .collect(toList());
+        log.info("getAll for user {}", userId);
+        return repository.get(userId)
+                .values()
+                .stream()
+                .filter(filter)
+                .sorted(comparator)
+                .collect(toList());
     }
 
 }
--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.topjava.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -12,37 +15,43 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 @Service
 public class MealService {
-
+    private static final Logger log = LoggerFactory.getLogger(MealService.class);
     private final MealRepository repository;
 
+    @Autowired
     public MealService(MealRepository repository) {
         this.repository = repository;
     }
 
     public Meal create(int userId, Meal meal) {
+        log.info("create {}", meal);
         return repository.save(userId, meal);
     }
 
     public void update(int userId, Meal meal) {
         checkNotFoundWithId(repository.save(userId, meal), meal.getId());
+        log.info("update {}", meal);
     }
 
     public void delete(int userId, int id) {
+        log.info("delete by id {}", id);
         checkNotFoundWithId(repository.delete(userId, id), id);
     }
 
     public Meal get(int userId, int id) {
+        log.info("get {}", id);
         return checkNotFoundWithId(repository.get(userId, id), id);
     }
 
     public Collection<Meal> getAllByFilter(int userId, LocalDate startDate, LocalDate endDate) {
-
+        log.info("getByFilter -> from {} to {}", startDate, endDate);
         //Get records from BD between dates
         return repository.getByFilter(userId, meal ->
                 DateTimeUtil.isBetweenInclusive(meal.getDate(), startDate, endDate));
     }
 
     public Collection<Meal> getAll(int userId) {
+        log.info("getAll for user #{}", userId);
         return repository.getAll(userId);
     }
 
