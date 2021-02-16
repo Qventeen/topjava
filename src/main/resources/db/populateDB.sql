1b9b666333a7fb17373615b58954fff037371a63
Sergey Tugaenko <qventeen@gmail.com>
2021-02-16 10:40:46 +0200

    4_2 Add Meals to initDB scripts

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,9 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, '2020-JANUARY-30 10:00','Завтрак', 800),
+       (100001, '2020-JANUARY-31 15:00','Обед', 300),
+       (100001, '2020-JANUARY-30 12:00','Обед', 1500),
+       (100000, '2020-JANUARY-31 13:00','Ужин', 100);
