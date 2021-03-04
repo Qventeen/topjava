46b3b359fe2bd151c13763baedc055effd33c30f
Sergey Tugaenko <qventeen@gmail.com>
2021-03-04 19:45:59 +0200

    HW04 Optional @Rule and TestTiming

---
+++ src/test/java/ru/javawebinar/topjava/TestTimer.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.topjava;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public class TestTimer implements TestRule{
+    private static final Logger log = LoggerFactory.getLogger(TestTimer.class);
+
+    @Override
+    public Statement apply(Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                Instant start = Instant.now();
+                try {
+                    base.evaluate();
+                }finally {
+                    Instant end  = Instant.now();
+                    Duration duration = Duration.between(start, end);
+                    log.debug("--> TIME OF TEST {} = {}ms", description, duration.toMillis());
+                }
+            }
+        };
+    }
+}
